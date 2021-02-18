ac127bf80721c21e3400f0f45c4e1e8f78e7eb46
Chunqiao Jiang <you@example.com>
2021-02-18 22:50:12 +0800

    element ui 实现分页

--- photos/server/lib/getPhotos.js
+++ photos/server/lib/getPhotos.js
@@ -1,14 +1,27 @@
 const { query } = require("./db")
+const defaultPage = 4
 
 module.exports = async (ctx, next) => {
-  console.log('params',ctx.query.uid)
-  const res = await query("select * from `photos` where uid=?",[ctx.query.uid])
+  console.log('params', ctx.query.uid, ctx.query.page)
+  const currentPage = ctx.query.page
+  if (!currentPage) {
+    currentPage = 1;
+  }
+  console.log('curre',currentPage)
+  const offset = (currentPage - 1) * defaultPage
+  console.log('offset',offset)
+  const sql = `SELECT * FROM photos WHERE uid=? LIMIT ?,?`;
+  const res = await query(sql,[ctx.query.uid,offset,defaultPage] )
+  const pageInfo = await getPageInfo(ctx.query.uid,currentPage)
   // console.log('photos:', res)
   if (res.length > 0) {
     ctx.body = {
       code: 0,
       msg: "查找图片成功",
-      data: res
+      data: {
+        res,
+        pageInfo
+      }
     }
   } else {
     ctx.body = {
@@ -16,4 +29,15 @@ module.exports = async (ctx, next) => {
       msg:"查找图片失败/没有图片"
     }
   }
+}
+
+async function getPageInfo (uId, currentPage) {
+  const sql = `SELECT * FROM photos WHERE uId=? `;
+  const res = await query(sql, [uId]);
+  console.log('pageres',res)
+  return {
+    prepage: defaultPage,
+    page: Number(currentPage),
+    total: res.length,
+  };
 }
